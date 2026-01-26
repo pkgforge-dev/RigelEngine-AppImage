9d87b282b647dbc8b1634dadb89853fa51e9b7b9
Link4Electronics <link4electronics@gmail.com>
2026-01-26 08:19:00 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/rigelengine_128.png
 export DESKTOP=/usr/share/applications/rigelengine.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/
