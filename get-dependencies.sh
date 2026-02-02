ca9004a17f7adc182f555b9f0c74a0907657635d
Link4Electronics <link4electronics@gmail.com>
2026-02-02 07:30:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,39 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    cmake      \
+    libdecor   \
+    sdl2_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package
+make-aur-package sdl2
 
 # If the application needs to be manually built that has to be done down here
+echo "Building RigelEngine..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/lethal-guitar/RigelEngine"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+	git clone "$REPO" ./RigelEngine
+echo "$VERSION" > ~/version
+
+cd ./RigelEngine
+mkdir -p build 
+cd build
+cmake .. -Wno-dev -DBUILD_TESTS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+make -j$(nproc)
+
+mkdir -p "/usr/bin"
+cp "build/src/RigelEngine" "/usr/bin"
+# copy over the destop file from the dist directory
+mkdir -p "/usr/share/applications"
+cp dist/linux/rigelengine.desktop "/usr/share/applications"
+# copy over the icons from the dist directory
+mkdir -p "/usr/share/icons"
+cp dist/linux/rigelengine_128.png "/usr/share/icons"
+# add icon path to the desktop entry
+echo Icon=/usr/share/icons/rigelengine_128.png >> "/usr/share/applications/rigelengine.desktop"
