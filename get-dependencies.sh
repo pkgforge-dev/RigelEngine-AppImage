c9a0fcc503b8f1e9eb86e05df47d4b625de7112c
Link4Electronics <link4electronics@gmail.com>
2026-02-02 09:16:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,16 +19,41 @@ get-debloated-pkgs --add-common --prefer-nano
 make-aur-package sdl2
 
 # If the application needs to be manually built that has to be done down here
-echo "Building nightly build of RigelEngine..."
+
+echo "Building RigelEngine..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/lethal-guitar/RigelEngine"
-# Get the latest tag
-TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/lethal-guitar/RigelEngine | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')
-# Get the short hash
-HASH=$(git ls-remote "$REPO" HEAD | cut -c 1-8)
-VERSION="${TAG}-${HASH}"
-git clone --recursive "$REPO" ./RigelEngine
+GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
+# Determine to build nightly or stable
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+	echo "Making nightly build of RigelEngine..."
+	# Get the latest tag
+    TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/lethal-guitar/RigelEngine | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')
+    # Get the short hash
+    HASH=$(git ls-remote "$REPO" HEAD | cut -c 1-8)
+    VERSION="${TAG}-${HASH}"
+    git clone --recursive "$REPO" ./RigelEngine
+else
+	echo "Making stable build of RigelEngine..."
+	wget "$GRON" -O ./gron.awk
+	chmod +x ./gron.awk
+	VERSION=$(wget https://api.github.com/repos/lethal-guitar/RigelEngine/tags -O - | \
+		./gron.awk | grep -v "nJoy" | awk -F'=|"' '/name/ {print $3}' | \
+		sort -V -r | head -1)
+	git clone --branch "$VERSION" --single-branch --recursive "$REPO" ./RigelEngine
+fi
 echo "$VERSION" > ~/version
+
+#echo "Building nightly build of RigelEngine..."
+#echo "---------------------------------------------------------------"
+
+# Get the latest tag
+#TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/lethal-guitar/RigelEngine | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')
+# Get the short hash
+#HASH=$(git ls-remote "$REPO" HEAD | cut -c 1-8)
+#VERSION="${TAG}-${HASH}"
+#git clone --recursive "$REPO" ./RigelEngine
+#echo "$VERSION" > ~/version
 
 cd ./RigelEngine
 mkdir -p build 
