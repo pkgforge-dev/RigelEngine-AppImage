300dac6484ba1e2cfc494dddd01eb4f1c10c7a83
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:07:05 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,15 +5,19 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/lethal-guitar/RigelEngine/refs/heads/master/dist/linux/rigelengine_128.png
+export DESKTOP=https://raw.githubusercontent.com/lethal-guitar/RigelEngine/refs/heads/master/dist/linux/rigelengine.desktop
 export STARTUPWMCLASS=RigelEngine
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/RigelEngine
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
